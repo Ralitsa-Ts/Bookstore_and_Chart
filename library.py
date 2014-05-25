41b1ac2770c3153e1ae329c0f0bac9822db30d5b
Ralitsa-Ts <rally_93@mail.bg>
2014-05-25 17:33:46 +0300

    I updated the module library.py

--- library.py
+++ library.py
@@ -2,41 +2,51 @@ from book_database import Book, BookDataBase
 
 
 class Library:
+
     def __init__(self, storage, genres):
         self.storage = storage
         self.genres = genres
         self.database = BookDataBase(self.storage)
-        self.books = self.database.booklist()
-        self.number_of_books = len(self.books)
-        Library.create_genre_dict(self.books, self.genres)
+        self._books = self.database.booklist()
+        self.number_of_books = len(self._books)
+        self.create_genre_dict()
+
+    @property
+    def books(self):
+        return self._books
 
     def add_book(self, book):
-        self.database(add_record)
-        self.books.append(book)
+        self.database.add_record(book)
+        if book in self._books:
+            index = self._books.index(book)
+            new_copies = book.number_of_copies
+            self._books[index].increase_number_of_copies(new_copies)
+        else:
+            self._books.append(book)
         self.number_of_books += 1
 
     def remove_book(self, book):
-        if book in self.books:
-            self.database(remove_record(book))
-            self.books.remove(book)
+        if book in self._books:
+            self.database.remove_record(book)
+            self._books.remove(book)
             self.number_of_books -= 1
 
     def book_information_by_title(self, title):
-        return [book for book in self.books if book.title() == title]
+        return [book for book in self._books if book.title == title]
 
     def book_information_by_author(self, author):
-        return [book for book in self.books if book.author() == author]
+        return [book for book in self._books if book.author == author]
 
     def number_of_different_books(self):
         return self.number_of_books
 
     def number_of_books_by_genres(self):
-        return {genre: len(books) for genre, books in genre_dict.items()}
+        return {genre: len(books) for genre, books in self.genre_dict.items()}
 
-    @staticmethod
-    def create_genre_dict(books, genres):
+    def create_genre_dict(self):
         self.genre_dict = dict()
-        for genre in genres:
+        books = self._books
+        for genre in self.genres:
             books_by_genre = [book for book in books if book.genre == genre]
             sorting = lambda x: x.title
-            genre_dict[genre] = sorted(book_by_genre, key=sorting)
+            self.genre_dict[genre] = sorted(books_by_genre, key=sorting)
