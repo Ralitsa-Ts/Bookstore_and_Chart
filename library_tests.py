f654db6814f8cfdf8d22350f0f5ced4061ae4ec2
Ralitsa-Ts <rally_93@mail.bg>
2014-05-25 15:47:53 +0300

    I updated the module library_tests.py

---
+++ library_tests.py
@@ -0,0 +1,25 @@
+from library import Library, Book
+import unittest
+
+
+class LibraryTest(unittest.TestCase):
+    def test_add_book(self):
+        pass
+
+    def test_remove_book(self):
+        pass
+
+    def test_book_information_by_title(self):
+        pass
+
+    def test_book_information_by_author(self):
+        pass
+
+    def test_number_of_different_books(self):
+        pass
+
+    def test_number_of_books_by_genres(self):
+        pass
+
+    def test_create_genre_dict(self):
+        pass
