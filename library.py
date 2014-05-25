249c6558a50a57457f01fc8294e13eb1cbef75e6
Ralitsa-Ts <rally_93@mail.bg>
2014-05-25 03:23:31 +0300

    I added the module library.py

---
+++ library.py
@@ -0,0 +1,24 @@
+from book_database import Book, BookDataBase
+
+
+class Library:
+    def __init__(self):
+        pass
+
+    def add_book(self, Book):
+        pass
+
+    def remove_book(self, Book):
+        pass
+
+    def book_information_by_title(self, title):
+        pass
+
+    def book_information_by_author(self, author):
+        pass
+
+    def number_of_different_books(self):
+        pass
+
+    def number_of_books_by genres(self):
+        pass
