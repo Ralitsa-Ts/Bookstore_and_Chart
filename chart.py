fbe86622d98289f1aaf5fee39df1e0828340c6e2
Ralitsa-Ts <rally_93@mail.bg>
2014-06-16 21:41:31 +0300

    I updated the module chart.py

--- chart.py
+++ chart.py
@@ -1,13 +1,11 @@
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 sys.path.append("model")
 from library import Library
 from PyQt4 import QtGui, QtCore
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as Fig
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as Nav
-from random import shuffle
 
 
 class Navigation(Nav):
@@ -22,12 +20,9 @@ class Window(QtGui.QDialog):
         self.figure = plt.figure(1, facecolor="white")
         self.canvas = Fig(self.figure)
         self.toolbar = Navigation(self.canvas, self)
-        self.button = QtGui.QPushButton('Plot')
-        self.button.clicked.connect(self.pie)
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
         self.setLayout(layout)
 
     def plot(self):
@@ -42,7 +37,6 @@ class Window(QtGui.QDialog):
         self.get_data()
         pie_data = {k: v for k, v in self.data.items() if v != 0}
         count = list(pie_data.values())
-        print(count)
         ax = self.figure.add_subplot(111)
         labels = pie_data.keys()
         ax.pie(count, labels=labels, labeldistance=1.2, autopct='%1.1f%%')
@@ -62,18 +56,23 @@ class Chart(QtGui.QWidget):
     def InitUI(self):
         self.pie_chart = QtGui.QRadioButton("pie chart")
         self.line_chart = QtGui.QRadioButton("line chart")
+        self.generate = QtGui.QPushButton('Generate')
+        self.generate.clicked.connect(self.show_data)
+        self.generate.setMinimumSize(80, 30)
 
         self.pie_chart.setChecked(True)
         box = QtGui.QVBoxLayout()
         box.addWidget(self.pie_chart)
         box.addWidget(self.line_chart)
+        box.addWidget(self.generate)
         label = QtGui.QLabel("Choose your type of chart:")
-        self.dialog = Window()
+        self.figure = Window()
 
         grid = QtGui.QGridLayout()
         grid.addWidget(label, 0, 0)
         grid.addLayout(box, 0, 1)
-        grid.addWidget(self.dialog, 1, 2)
+        grid.addWidget(self.figure, 1, 2)
+
         grid.setColumnStretch(0, 2)
         grid.setColumnStretch(1, 2)
         grid.setRowStretch(1, 2)
@@ -81,3 +80,9 @@ class Chart(QtGui.QWidget):
         vBoxlayout = QtGui.QVBoxLayout()
         vBoxlayout.addLayout(grid)
         self.setLayout(vBoxlayout)
+
+    def show_data(self):
+        if self.pie_chart.isChecked():
+            self.figure.pie()
+        else:
+            self.figure.plot()
