a358f57195df53ba5e5e5b993439b43399f7987f
Ralitsa-Ts <rally_93@mail.bg>
2014-06-16 20:11:02 +0300

    I updated the module library.py

--- model/library.py
+++ model/library.py
@@ -2,13 +2,16 @@
     This module contains a class named Library,which provides
     manipulating a virtual bookstore.
 """
-from book_database import Book, BookDataBase
 import re
 
+from book_database import Book, BookDataBase
+STORAGE = "database.txt"
+GENRES = ["Fantasy", "Drama", "Romance", "Thriller"]
 
 class Library:
 
-    def __init__(self, storage, genres):
+    @staticmethod
+    def __init__(storage=None, genres=None):
         """
             Setting the basic information about the library.
             Keyword arguments:
@@ -17,116 +20,131 @@ class Library:
             We also extract all records from the file given and create a list
             of the books and get their amount.
         """
-        self.storage = storage
-        self.genres = genres
-        self.database = BookDataBase(self.storage)
-        self._books = self.database.booklist()
-        self.number_of_books = len(self._books)
-        self.create_genre_dict()
+        if storage is None:
+            Library.storage = STORAGE
+        else:
+            Library.storage = storage
+        if genres is None:
+            Library.genres = GENRES
+        else:
+            Library.genres = genres
+        Library.database = BookDataBase(Library.storage)
+        Library.books = Library.database.booklist()
+        Library.number_of_books = len(Library.books)
+        Library.create_genre_dict()
 
-    @property
-    def books(self):
-        """
-            Getting the list of books in the library.
-        """
-        return self._books
 
-    def add_book(self, book):
+    @staticmethod
+    def add_book(book):
         """
             Add a new book to the library.If it already exists then the number
             of copies of the existing book is increased.
         """
-        self.database.add_record(book)
-        if book in self._books:
-            index = self._books.index(book)
+        Library.database.add_record(book)
+        if book in Library.books:
+            index = Library.books.index(book)
             new_copies = book.number_of_copies
-            self._books[index].increase_number_of_copies(new_copies)
+            Library.books[index].increase_number_of_copies(new_copies)
         else:
-            self._books.append(book)
-            self.number_of_books += 1
-            self.genre_dict[book.genre].append(book)
+            Library.books.append(book)
+            Library.number_of_books += 1
+            Library.genre_dict[book.genre].append(book)
 
-    def remove_book(self, book):
+    @staticmethod
+    def remove_book(book):
         """
             Remove a book from the library if it exists.
         """
-        if book in self._books:
-            self.database.remove_record(book)
-            self._books.remove(book)
-            self.number_of_books -= 1
-            self.genre_dict[book.genre].remove(book)
+        if book in Library.books:
+            Library.database.remove_record(book)
+            Library.books.remove(book)
+            Library.number_of_books -= 1
+            Library.genre_dict[book.genre].remove(book)
 
-    def book_information_by_title(self, title):
+    @staticmethod
+    def book_information_by_title(title):
         """
             Returns a list of all books with the given title.
         """
-        return [book for book in self._books if re.search(title, book.title)]
+        return [book for book in Library.books if re.search(title, book.title)]
 
-    def book_information_by_author(self, author):
+    @staticmethod
+    def book_information_by_author(author):
         """
             Returns a list of all books with the given author.
         """
-        return [book for book in self._books if re.search(author, book.author)]
+        return [book for book in Library.books if
+                re.search(author, book.author)]
 
-    def number_of_different_books(self):
+    @staticmethod
+    def number_of_different_books():
         """
             Return the number of different books in the library.
         """
-        return self.number_of_books
+        return Library.number_of_books
 
-    def number_of_books_by_genres(self):
+    @staticmethod
+    def number_of_books_by_genres():
         """
             Returns a dictionary with keys - the possible genres and values -
             the amount of books in the corresponding genres.
         """
-        return {genre: len(books) for genre, books in self.genre_dict.items()}
+        return {genre: len(books) for genre, books in
+                Library.genre_dict.items()}
 
-    def create_genre_dict(self):
+    @staticmethod
+    def create_genre_dict():
         """
             Creates a dictionary with keys - the possible genres and values -
             lists of books in the corresponding genres.
         """
-        self.genre_dict = dict()
-        books = self._books
-        for genre in self.genres:
+        Library.genre_dict = dict()
+        books = Library.books
+        for genre in Library.genres:
             books_by_genre = [book for book in books if book.genre == genre]
             sorting = lambda x: x.title
-            self.genre_dict[genre] = sorted(books_by_genre, key=sorting)
+            Library.genre_dict[genre] = sorted(books_by_genre, key=sorting)
 
-    def remove_all_data(self):
+    @staticmethod
+    def remove_all_data():
         """
             Removes all records of books in the given storage.
         """
         pass
 
-    def manipulation(self, book):
+    @staticmethod
+    def manipulation(book):
         remove_book(book)
         add_book(book)
 
-    def return_book(self, book):
+    @staticmethod
+    def return_book(book):
         """
             Returns a copy of the book back to the library.
         """
         book.increase_number_of_copies(1)
         manipulation(book)
 
-    def take_book(self, book):
+    @staticmethod
+    def take_book(book):
         """
             Takes a copy of the book from the library.
         """
         book.decrease_number_of_copies(1)
         manipulation(book)
 
-    def like_book(self, book):
+    @staticmethod
+    def like_book(book):
         """
             Increases the book's rating.
         """
         book.increase_rating()
         manipulation(book)
 
-    def dislike_book(self, book):
+    @staticmethod
+    def dislike_book(book):
         """
-            Increases the book's rating.
+            Decreases the book's rating.
         """
         book.decrease_rating()
         manipulation(book)
