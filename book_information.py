9113950a12c7d2497c4ace9fbe60e7ccfcfb8da5
Ralitsa-Ts <rally_93@mail.bg>
2014-06-20 19:52:59 +0300

    I updated the module

--- book_information.py
+++ book_information.py
@@ -3,6 +3,7 @@ import os
 sys.path.append("model")
 from library import Library, Book
 from PyQt4 import QtGui, QtCore
+from functools import partial
 FOLDER = os.path.normpath("images/folder.jpg")
 
 
@@ -52,6 +53,37 @@ class BookInformation(QtGui.QWidget):
         vBoxlayout.addLayout(self.grid)
         self.setLayout(vBoxlayout)
 
+    def generate_book_by_row(self, row):
+        record = ""
+        for i in range(5):
+            data = self.table.item(row, i).text()
+            record += ("+" + data)
+        print(record[1:])
+
+    def get_a_copy(self, row):
+        value = int(self.table.item(row, 4).text()) - 1
+        if value >= 0:
+            item = QtGui.QTableWidgetItem(str(value))
+            self.table.setItem(row, 4, item)
+
+    def return_a_copy(self, row):
+        self.generate_book_by_row(row)
+        value = int(self.table.item(row, 4).text()) + 1
+        item = QtGui.QTableWidgetItem(str(value))
+        self.table.setItem(row, 4, item)
+
+    def like_a_book(self, row):
+        #Library.like_book()
+        value = "%.2f" % (float(self.table.item(row, 3).text()) + 0.1)
+        item = QtGui.QTableWidgetItem(str(value))
+        self.table.setItem(row, 3, item)
+
+    def dislike_a_book(self, row):
+        #Library.dislike_book()
+        value = "%.2f" % (float(self.table.item(row, 3).text()) - 0.1)
+        item = QtGui.QTableWidgetItem(str(value))
+        self.table.setItem(row, 3, item)
+
     def update_table(self):
         self.table.setRowCount(0)
         self.table.setColumnCount(0)
@@ -62,16 +94,16 @@ class BookInformation(QtGui.QWidget):
             results = Library.book_information_by_title(search)
         else:
             results = Library.book_information_by_title_author(search)
-        self.table = QtGui.QTableWidget(len(results), 9, self)
+        self.table = QtGui.QTableWidget(len(results), 10, self)
         headers = ("Title", "Author's name", "Published in", "Rating",
-                   "Copies", "Genre", "Get", "Return", "Vote")
+                   "Copies", "Genre", "Get", "Return", "Like"," Dislike")
         self.table.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
         self.table.setHorizontalHeaderLabels(headers)
         self.table.resizeColumnsToContents()
         self.table.resizeRowsToContents()
 
-        widths = [160, 150, 90, 60, 70, 90, 60]
-        for col, width in zip(range(7), widths):
+        widths = [140, 140, 90, 60, 70, 80, 50, 60, 50, 60]
+        for col, width in zip(range(9), widths):
             self.table.setColumnWidth(col, width)
 
         self.table.horizontalHeader().setStretchLastSection(True)
@@ -85,11 +117,22 @@ class BookInformation(QtGui.QWidget):
                 col += 1
 
         for row in range(len(results)):
-            btn = QtGui.QPushButton("Get")
-            btn.setMinimumSize(50, 30)
-            btn2 = QtGui.QPushButton("Return")
-            btn2.setMinimumSize(50, 30)
-            self.table.setCellWidget(row, 6, btn)
-            self.table.setCellWidget(row, 7, btn2)
+            get_btn = QtGui.QPushButton("Get")
+            get_btn.setMinimumSize(50, 30)
+            get_btn.clicked.connect(partial(self.get_a_copy, row))
+            self.table.setCellWidget(row, 6, get_btn)
+            return_btn = QtGui.QPushButton("Return")
+            return_btn.setMinimumSize(50, 30)
+            return_btn.clicked.connect(partial(self.return_a_copy, row))
+            self.table.setCellWidget(row, 7, return_btn)
+
+            like_btn = QtGui.QPushButton("Like")
+            like_btn.setMinimumSize(50, 30)
+            like_btn.clicked.connect(partial(self.like_a_book, row))
+            self.table.setCellWidget(row, 8, like_btn)
+            dislike_btn = QtGui.QPushButton("Dislike")
+            dislike_btn.setMinimumSize(50, 30)
+            dislike_btn.clicked.connect(partial(self.dislike_a_book, row))
+            self.table.setCellWidget(row, 9, dislike_btn)
 
         self.grid.addWidget(self.table, 1, 0)
